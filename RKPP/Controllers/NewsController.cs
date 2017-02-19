3ca160d32fcab69a9ce6585b9dc20801301ad865
pszczelarz <rkpp8@o2.pl>
2017-02-19 16:38:40 +0100

    Add NewsController and scaffolding.

---
+++ RKPP/Controllers/NewsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RKPP.Models;
+
+namespace RKPP.Controllers
+{
+    public class NewsController : Controller
+    {
+        private NewDBContext db = new NewDBContext();
+
+        // GET: News
+        public ActionResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+
+        // GET: News/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            New @new = db.Movies.Find(id);
+            if (@new == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@new);
+        }
+
+        // GET: News/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: News/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Content,Date")] New @new)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(@new);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(@new);
+        }
+
+        // GET: News/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            New @new = db.Movies.Find(id);
+            if (@new == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@new);
+        }
+
+        // POST: News/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Content,Date")] New @new)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@new).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(@new);
+        }
+
+        // GET: News/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            New @new = db.Movies.Find(id);
+            if (@new == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@new);
+        }
+
+        // POST: News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            New @new = db.Movies.Find(id);
+            db.Movies.Remove(@new);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

5a664f67f34f1426d5e1400f02c1cb0742b7a48f
pszczelarz <rkpp8@o2.pl>
2017-02-22 13:22:08 +0100

    Add the Plan model.

---
+++ RKPP/Models/Plan.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace RKPP.Models
+{
+    public class Plan
+    {
+        public int Id { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+    }
+
+    public class PlanDBContext : DbContext
+    {
+        public DbSet<Plan> Plans { get; set; }
+    }
+}

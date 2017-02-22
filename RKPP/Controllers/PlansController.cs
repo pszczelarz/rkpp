98ff5c14fe7c6b59eca7fe3a1efe49592b7301e9
pszczelarz <rkpp8@o2.pl>
2017-02-22 18:05:33 +0100

    Hide the unused methods.

--- RKPP/Controllers/PlansController.cs
+++ RKPP/Controllers/PlansController.cs
@@ -32,27 +32,27 @@ namespace RKPP.Controllers
         }
 
         // GET: Plans/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
         // POST: Plans/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Content")] Plan plan)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Plans.Add(plan);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Id,Content")] Plan plan)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Plans.Add(plan);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
 
-            return View(plan);
-        }
+        //    return View(plan);
+        //}
 
         // GET: Plans/Edit/5
         public ActionResult Edit(int? id)
@@ -86,38 +86,38 @@ namespace RKPP.Controllers
         }
 
         // GET: Plans/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Plan plan = db.Plans.Find(id);
-            if (plan == null)
-            {
-                return HttpNotFound();
-            }
-            return View(plan);
-        }
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Plan plan = db.Plans.Find(id);
+        //    if (plan == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(plan);
+        //}
 
         // POST: Plans/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Plan plan = db.Plans.Find(id);
-            db.Plans.Remove(plan);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Plan plan = db.Plans.Find(id);
+        //    db.Plans.Remove(plan);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
     }
 }
